8281ec5b001e301705f86d7a778f9b8a6b1771ef
karanhimadri <karanhimadri1234@gmail.com>
2026-01-11 00:55:03 +0530

    Fix Selenium headless chrome for Github Actions CI

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,23 +4,38 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
+
   protected WebDriver driver;
   protected ConfigReader config;
 
   @BeforeMethod
   public void setUp() {
     System.out.println(">>> BASE TEST SETUP EXECUTED <<<");
+
     config = new ConfigReader();
 
-    driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicite Wait used
+    // Automatically manages ChromeDriver (CI-safe)
+    WebDriverManager.chromedriver().setup();
+
+    ChromeOptions options = new ChromeOptions();
+
+    // Required for GitHub Actions (Linux)
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--window-size=1920,1080");
+
+    driver = new ChromeDriver(options);
+
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.get(config.getProperty("url"));
   }
 
